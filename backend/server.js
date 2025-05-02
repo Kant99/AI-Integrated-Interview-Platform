a0e1d5c5af2d409e29546c451691c1fcd1a40771
Kant99 <reshukumar123@gmail.com>
2025-05-03 01:57:05 +0530

    Gemini API tested and added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,45 @@
-import app from "./src/app.js";
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+import { GoogleGenAI } from '@google/genai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const API_KEY = process.env.GEMINI_API_KEY;
+
+
+
+const ai = new GoogleGenAI({ apiKey: API_KEY });
+
+try {
+    const prompt = `
+Act like an interviewer and generate 5 technical interview questions for the following candidate:
+
+Role: "Frontend Developer"
+Tech Stack: "MERN Stack"
+Experience: 3 years
+    `;
+
+    
+    const result = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: prompt,
+    });
+    console.log(result.text);
+
+    
+  } catch (error) {
+    console.error("Error generating questions:", error.message);
+    
+  }
+
+
+//   const ai = new GoogleGenAI({ apiKey: "YOUR_API_KEY" });
+
+//   async function main() {
+//     const response = await ai.models.generateContent({
+//       model: "gemini-2.0-flash",
+//       contents: "Explain how AI works in a few words",
+//     });
+//     console.log(response.text);
+//   }
+  
+//   main();
